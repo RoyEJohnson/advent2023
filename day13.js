d49a19b995c6b5bb4808e46bbdf4671374a9b2ec
Roy Johnson <roy.e.johnson@rice.edu>
2023-12-14 07:48:10 -0600

    Day 13 part 2

--- day13.js
+++ day13.js
@@ -10,6 +10,27 @@ console.info(`D${day}P2 ${file}:`, part2(`./day${day}${file}.txt`));
 file = 'input';
 console.info(`D${day}P2 ${file}:`, part2(`./day${day}${file}.txt`));
 
+// runDebug('./day13debug.txt');
+
+function runDebug(file) {
+    const pairs = fetchData(file, false).split(/\n\n\n/).map(p => p.split(/\n\n/).map(g => g.split(/\n/)));
+
+    for (const [after, before] of pairs.slice(11, 12)) {
+        const myFix = fixSmudgeAndScore(before);
+        const vertical = myFix > 100;
+        const reflectionRow = vertical ? myFix / 100 : myFix;
+        const matches = verify(reflectionRow - 1, vertical ? after : transpose(after));
+
+        if (matches) {
+            console.info('...ok...', vertical && 'vertical');
+        } else {
+            console.info('!! mismatch:', {
+                vertical, myFix, grid: after.join('\n'), xpose: transpose(after).join('\n')
+            });
+        }
+    }
+}
+
 function part1(file) {
     const grids = fetchData(file, false).split(/\n\n/).map(g => g.split(/\n/));
 
@@ -22,6 +43,10 @@ function part2(file) {
     return sum(grids.map(fixSmudgeAndScore));
     // 39565 is too high
     // 36669 is too low
+    // totalHorizontal: 371
+    // totalVertical: 378
+    // result: 37478
+
 }
 
 function score(grid) {
@@ -41,7 +66,8 @@ function reflectionRow(grid) {
 }
 
 function verify(c, grid) {
-    for (let i = 1; c - i >= 0 && c + i + 1< grid.length; ++i) {
+    for (let i = 0; c - i >= 0 && c + i + 1< grid.length; ++i) {
+        // console.info('?? Compare', {n1: c-i, n2: c+i+1, line1: grid[c-i], line2: grid[c+i+1]});
         if (grid[c-i] !== grid[c+i+1]) {
             // console.info('Failed match:', grid[c-i], grid[c+i]);
             return false;
@@ -63,7 +89,7 @@ function fixSmudgeAndScore(grid) {
         const rowC = smudgeCandidate(grid, line, +index);
 
         if (rowC) {
-            // console.info('** Smudge row', rowC, index);
+            // console.info('** Smudge row', rowC, +index);
             return 100 * (+index + rowC + 1) / 2;
         }
     }
@@ -74,7 +100,7 @@ function fixSmudgeAndScore(grid) {
         const colC = smudgeCandidate(transposedGrid, line, +index);
 
         if (colC) {
-            console.info('*** Smudge col', colC, index);
+            // console.info('*** Smudge col', colC, +index);
             return (+index + colC + 1) / 2;
         }
     }
@@ -85,8 +111,14 @@ function fixSmudgeAndScore(grid) {
 
 function smudgeCandidate(grid, line, index) {
     const candidates = findPossibleSmudges(grid, line, index);
+    const verifiedCandidate = candidates.find(c => verifyFix(grid, index, c));
 
-    return candidates.find(c => verifyFix(grid, index, c));
+    // if (verifiedCandidate) {
+    //     console.info('** Verified', line, verifiedCandidate);
+    // } else {
+    //     console.info('-- Cannot verify');
+    // }
+    return verifiedCandidate;
 }
 
 function findPossibleSmudges(grid, line, index) {
@@ -94,6 +126,7 @@ function findPossibleSmudges(grid, line, index) {
 
     for (let i2=+index + 1; i2 < grid.length; i2 += 2) {
         if (charDiff(line, grid[i2]) === 1) {
+            // console.info('** Char diff is 1', line, grid[i2], +index, i2);
             candidates.push(i2);
         }
     }
@@ -105,8 +138,8 @@ function verifyFix(grid, index, c) {
     const reflectionLine = (index + c - 1) / 2;
 
     if (verify(reflectionLine, fixedGrid)) {
-        // console.info('Fixed grid', fixedGrid);
-        // console.info('Reflection line', reflectionLine);
+        // console.info('Fixed grid', fixedGrid.join('\n'));
+        // console.info('Reflection line', reflectionLine, fixedGrid[reflectionLine]);
         return true;
     }
     return false;
